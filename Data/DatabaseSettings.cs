a6bde4a96efc44f0e4c78071a90de9a3d2f506e2
Can <uzdurancan@gmail.com>
2023-07-24 01:05:43 +0300

    Boilerplate is created and explanation of layers are added

--- Data/DatabaseSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace userMS.Data
-{
-    public class DatabaseSettings
-    {
-        public string ConnectionString { get; set; }
-        public string DatabaseName { get; set; }
-        public string UserCollectionName { get; set; }
-    }
-}
